32f625778b35785255bf63d41c50f33cc01438f9
Trey Simmons <yert511@gmail.com>
2022-08-10 19:58:27 -0500

    Add index page and functionality

--- pages/colors.js
+++ pages/colors.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import GameArea from '../components/game-comps/game-area';
 
 export default function Colors() {
-  let totalReps = 5;
-  let timeInterval = 3;
+  const [settings, setSettings] = useState({
+    "mode": "",
+    "reps": "",
+    "interval": ""
+  });
+  const router = useRouter();
+  const query = router.query;
+
+  useEffect(() => {
+    const localMode = window.localStorage.getItem("mode");
+    const localReps = window.localStorage.getItem("reps");
+    const localInterval = window.localStorage.getItem("interval");
+
+    if(localMode === null) {
+      setSettings({"mode": query.mode, "reps": query.reps, "interval": query.interval});
+      window.localStorage.setItem("mode", query.mode);
+      window.localStorage.setItem("reps", query.reps);
+      window.localStorage.setItem("interval", query.interval);
+    }else{
+      setSettings(({
+        "mode": localMode,
+        "reps": localReps,
+        "interval": localInterval
+      }));
+    };
+  }, []);
 
   return(
     <>
       <Head>
         <title>Identify the Color</title>
       </Head>
-      <GameArea gameMode="colors" totalReps={totalReps} timeInterval={timeInterval} />
+      <GameArea gameMode={settings["mode"]} totalReps={settings["reps"]} timeInterval={settings["interval"]} />
     </>
   );
 };
